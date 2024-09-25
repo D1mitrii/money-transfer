000729dab100947df29eac8cebeebde8c4882725
unknown <dyusmirnov@stud.etu.ru>
2024-09-25 11:22:55 +0300

    fix: updated_at field

--- bank-service/internal/models/account.go
+++ bank-service/internal/models/account.go
@@ -7,9 +7,9 @@ import (
 )
 
 type Account struct {
-	UUID      uuid.UUID `db:"uuid"`
-	Name      string    `db:"account_name"`
-	Balance   int64     `db:"balance"`
-	CreatedAt time.Time `db:"created_at"`
-	UpdatedAt time.Time `db:"updated_at"`
+	UUID      uuid.UUID  `db:"uuid"`
+	Name      string     `db:"account_name"`
+	Balance   int64      `db:"balance"`
+	CreatedAt time.Time  `db:"created_at"`
+	UpdatedAt *time.Time `db:"updated_at"`
 }
